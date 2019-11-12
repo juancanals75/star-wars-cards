c8f67facd563d8108d90a139cf5ac614314c3ded
Juan Canals <juancanals75@gmail.com>
2019-11-12 05:29:05 -0300

    tried the search

--- src/CharList.js
+++ src/CharList.js
@@ -1,18 +1,55 @@
 import React from 'react'
 import Character from './Character'
 
-function CharList(props) {
+class CharList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      filtered: [],
+      searchTxt : ""
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
 
-  const allCharList = props.allPeople
+  componentDidMount() {
+    this.setState({filtered: this.props.allPeople})
+  }
 
-  return (
-    <div className="list-container">{allCharList.map(function(value) {
-      return (
-        <Character {...value} />
-      )
-    })}
-  </div>
-  )
+  handleChange(e) {
+    this.setState({searchTxt: e.target.value})
+    let currentList = []
+    let newList = []
+
+    if (e.target.value !== "") {
+      currentList = this.props.allPeople
+      newList = currentList.filter(item => {
+          // const lc = item.toLowerCase()
+          // const filter = e.target.value.toLowerCase()
+          item.contains(e.target.value)
+      })
+    } else {
+      newList = this.props.allPeople
+    }
+
+    this.setState({ filtered: newList})
+  }
+
+  render() {
+    const allCharList = this.state.filtered.map(function(charProps, index) { return (<Character key={index} {...charProps} />) })
+    return (
+      <div className="list-container">
+        <div className="list-results">
+          {allCharList}
+        </div>
+        <div className="search">
+          <input
+          name="search"
+          value={this.state.searchTxt}
+          onChange={this.handleChange}/>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default CharList
