52fd66b1e4e6427c876c79eeda11340db354dde3
Juan Canals <juancanals75@gmail.com>
2019-11-16 12:25:10 -0300

    Updates

---
+++ precache-manifest.9cc0f9706aa46a996e5436fa220d84d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f5c4f5ed63e80ef26653ffdbd4e409e",
+    "url": "/star-wars-cards/index.html"
+  },
+  {
+    "revision": "057940c7c8d9fefd99c3",
+    "url": "/star-wars-cards/static/css/main.9aced210.chunk.css"
+  },
+  {
+    "revision": "61b0604be0cacdc7d716",
+    "url": "/star-wars-cards/static/js/2.0c474ac0.chunk.js"
+  },
+  {
+    "revision": "057940c7c8d9fefd99c3",
+    "url": "/star-wars-cards/static/js/main.aae9f86f.chunk.js"
+  },
+  {
+    "revision": "cb680ef7cc68a2dd3853",
+    "url": "/star-wars-cards/static/js/runtime-main.42698bb9.js"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/star-wars-cards/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "c982ef251eeaa93f501080ca646e5e24",
+    "url": "/star-wars-cards/static/media/anakinmono.c982ef25.ttf"
+  }
+]);
